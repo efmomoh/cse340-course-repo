0485d8d260a8b38266b89ae070e475dbe3512346
efmomoh <efmomoh25@gmail.com>
2026-09-19 12:32:09 +0800

    Refactor and adds service project details route parameter functionality

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,12 +1,33 @@
-// Import any needed model functions 
-import { getAllProjects } from "../models/projects.js";
+// Import any needed model functions
+
+import {
+    getUpcomingProjects,
+    getProjectDetails
+} from "../models/projects.js";
+
+// Define the number of upcoming projects to display
+const NUMBER_OF_UPCOMING_PROJECTS = 5;
 
 // Define any controller functions for the projects page
 const showProjectsPage = async (req, res) => {
-    const projects = await getAllProjects();
-    const title = "Service Projects";
+    const projects = await getUpcomingProjects(NUMBER_OF_UPCOMING_PROJECTS);
+    const title = "Upcoming Service Projects";
+
     res.render("projects", { title, projects });
 };
 
-// Export any controller functions that need to be used in route.js file
-export { showProjectsPage };
+// Define the controller function for the project details page
+const showProjectDetailsPage = async (req, res) => {
+    const projectId = req.params.id;
+    const project = await getProjectDetails(projectId);
+    const title = "Project Details";
+
+    res.render("project", { title, project });
+
+};
+
+// Export any controller functions that need to be used in routes.js file
+export {
+    showProjectsPage,
+    showProjectDetailsPage
+};
